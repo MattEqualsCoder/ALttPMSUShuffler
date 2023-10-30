dbad416d744c6dcbbfc062b7df3051a5fd18f2a8
MattEqualsCoder <mattequalscoder@gmail.com>
2023-10-30 15:22:32 -0400

    Add path property

--- CreateBundle.ps1
+++ CreateBundle.ps1
@@ -2,6 +2,8 @@ $output = @()
 foreach ($file in Get-ChildItem "$PSScriptRoot\resources\snes"  -Recurse -Filter tracks.json)
 {
     $data = Get-Content -Raw -Path $file.fullname | ConvertFrom-Json
+    $filename = (Get-Item $file.FullName).Directory.Parent.Name
+    Add-Member -InputObject $data.meta -NotePropertyName "path" -NotePropertyValue "snes/$filename"
     $output += $data
 }
 $output | ConvertTo-Json -Depth 10 | Out-File -FilePath "$PSScriptRoot\msu_types.json"
