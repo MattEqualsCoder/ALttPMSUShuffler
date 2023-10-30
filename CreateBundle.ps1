91640b8c1629d8f5efe39ba233f187edc8ad8984
MattEqualsCoder <mattequalscoder@gmail.com>
2023-10-30 14:08:07 -0400

    Update github action

--- CreateBundle.ps1
+++ CreateBundle.ps1
@@ -5,3 +5,7 @@ foreach ($file in Get-ChildItem "$PSScriptRoot\resources\snes"  -Recurse -Filter
     $output += $data
 }
 $output | ConvertTo-Json -Depth 10 | Out-File -FilePath "$PSScriptRoot\msu_types.json"
+$output = git describe --tags --abbrev=0 | Out-String
+$output = [int]($output -replace "[^0-9\.]", "")
+$output = $output + 1
+Write-Output "tag=v$output.0" >> $env:GITHUB_OUTPUT
